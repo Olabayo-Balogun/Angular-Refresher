daa14352d5037437ab40f6158e06324ebc0b7203
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-09-07 17:12:37 +0100

    Bootstrapped an Angular component and Injected it into the index.html file.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+//We import the built component by calling the name we assigned to it in the "export class" code block within the component file.
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    //Here is where we declare that it will be used. Components that will be used in a project are usually declared here.
+    EventsAppComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  // This is where we declare that the component is one that is instantly used when it is called.
+  bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
---
+++ src/app/events-app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // The selector name "events-app" is what must be called when trying to render its content (which is usually found in the template) to where you want it.
+  selector: 'events-app',
+
+  //template typically contains the code snippet that should be used as a template wherever its injected.
+  template: '<h2>Hello World</h2>',
+})
+
+//The class must be exported for it to be useable/callable where you need it.
+export class EventsAppComponent {
+  title = 'app';
+}
