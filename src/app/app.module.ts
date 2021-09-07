c1e81540b14af2a7308833b8b5686281e1ebd09b
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-09-07 17:27:05 +0100

    Added comments to help explain more about the structure of the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,15 @@ import { EventsAppComponent } from './events-app.component';
     //Here is where we declare that it will be used. Components that will be used in a project are usually declared here.
     EventsAppComponent
   ],
+
+  //The "imports" array is where we declare modules that will be used thus making it's content available to the module it is declared in.
   imports: [
+    //The BrowserModule makes a lot of core angular services and directives available for us to use.
+    //imports makes things available for use in the declaration and other arrays so the import array is very important
     BrowserModule
   ],
+  
+  //Services are declared with the "providers" array.
   providers: [],
   // This is where we declare that the component is one that is instantly used when it is called.
   bootstrap: [EventsAppComponent]
