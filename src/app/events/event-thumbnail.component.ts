24f6e32df363656e55e144f24ad8aa86e8760afe
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-09-14 15:26:58 +0100

    Styled components with Angular class styling and ngClass

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -9,7 +9,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
     <div class="well hoverwell thumbnail">
       <h2>{{ event?.name }}</h2>
       <div>Date: {{ event?.date }}</div>
-      <div [ngSwitch]="event?.time">
+      <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
         Time: {{ event?.time }}
         <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
         <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -24,8 +24,15 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
       </div>
     </div>
   `,
+  //The "!important" is used to ensure that the style isn't overridden by any other styles.
   styles: [
     `
+      .green {
+        color: #003300 !important;
+      }
+      .bold {
+        font-weight: bold;
+      }
       .thumbnail {
         min-height: 210px;
       }
@@ -42,4 +49,27 @@ export class EventThumbnailComponent {
   //The code below basically creates an event property that is of type "any" because we're not particular about it's data type, it's like "var" in a way.
   //The "@Input()" method tells the project to expect data from another component.
   @Input() event: any;
+
+  //getStartTimeClass(){
+  //The "isEarlyStart" is what evaluates the condition while we declare the styling component that should accompany the style.
+  // const isEarlyStart = this.event && this.event.time === '8:00 am'
+  // return {green: isEarlyStart, bold: isEarlyStart}
+
+  //The code snippet above does the same thing as the code snippet below.
+  // getStartTimeClass() {
+  //   if (this.event && this.event.time === '8:00 am') {
+  //     return 'green bold';
+  //   } else {
+  //     return '';
+  //   }
+  // }
+
+  //The code snippet above does the same thing as the code snippet below
+  getStartTimeClass() {
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold'];
+    } else {
+      return [];
+    }
+  }
 }
